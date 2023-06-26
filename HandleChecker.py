e5b900f6cae827283a2b94f4e0cc2841fc9ca2ca
andysantanar <andysantana00@gmail.com>
2023-06-26 19:10:54 -0400

    Support for 6 social medias

--- HandleChecker.py
+++ HandleChecker.py
@@ -3,69 +3,11 @@ import requests
 import time
 
 sg.theme("default1")
-# links
-# https://www.facebook.com/
-# https://www.instagram.com/
-# https://www.tiktok.com/@
-# https://www.twitter.com/
-# https://www.pinterest.com/
-# https://www.youtube.com/@
-# https://www.twitch.tv/
-
-# status 200 list: all ok and have username displayed (unavailable to use)
-# https://www.facebook.com/LeBron
-# https://www.instagram.com/elonmusk
-# https://www.tiktok.com/@elonmusk
-# https://www.twitter.com/elonmusk
-# https://www.pinterest.com/elonmusk
-# https://www.youtube.com/@elonmusk
-# https://www.twitch.tv/elonmusk
-
-# doesn't exist : 
-# https://www.facebook.com/Leon52k0235      status 200, message: 
-                                            # This content isn't available right now
-                                            # When this happens, it's usually because 
-                                            # the owner only shared it with a small 
-                                            # group of people, changed who can see 
-                                            # it or it's been deleted.
-
-# https://www.instagram.com/Leon52k0235     status 200, message:
-                                            # Sorry, this page isn't available.
-                                            # The link you followed may be broken, 
-                                            # or the page may have been removed. 
-                                            # Go back to Instagram.
-
-# https://www.tiktok.com/@Leon52k0235       status 200, message:
-                                            # Couldn't find this account
-                                            # Looking for videos? Try browsing 
-                                            # our trending creators, hashtags, and sounds.
-
-# https://www.twitter.com/Leon52k0235       status 200, message:
-                                            #This account doesn’t exist
-                                            #Try searching for another.
-
-# https://www.pinterest.com/Leon52k0235     status 200, message:
-                                            # (no message)
-
-
-# https://www.youtube.com/@Leon52k0235      status 404, message:
-                                            #This page isn't available. Sorry about that.
-                                            #Try searching for something else.
-
-# https://www.twitch.tv/Leon52k0235         status 200, message:
-                                            #Sorry. Unless you’ve got a time machine, 
-                                            # that content is unavailable.
-
-
-
-
-
 
 base_urls = {
      'FaceBook': 'https://www.facebook.com/',
      'Instagram': 'https://www.instagram.com/',
      'TikTok': 'https://www.tiktok.com/@',
-     'Twitter': 'https://www.twitter.com/',
 
      'Pinterest': 'https://www.pinterest.com/',
      'YouTube': 'https://www.youtube.com/@',
@@ -78,7 +20,6 @@ layout = [
             [sg.Checkbox('FaceBook', key='FaceBook')],
             [sg.Checkbox('Instagram', key='Instagram')],
             [sg.Checkbox('TikTok', key='TikTok')],
-            [sg.Checkbox('Twitter', key='Twitter')]
         ], vertical_alignment='center', justification='center'),
 
         sg.Column([
@@ -107,6 +48,8 @@ def checkhandle(handle, key):
          return True
     if key == 'Pinterest' and 'user not found' in page_content.lower():
          return True
+    if key == 'Twitch' and 'href="https://www.twitch.tv/' not in page_content.lower():
+         return True
     elif response.status_code == 200:
         if handle.lower() in page_content.lower():
                    return False
@@ -119,9 +62,6 @@ def checkhandle(handle, key):
             return True
     else:
          return True
-
-              
-
 
 
 def print_selected(key):
