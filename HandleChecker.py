a619dc6cfde370ceba5184eebfe214c0c904dc02
andysantanar <andysantana00@gmail.com>
2023-06-26 23:46:11 -0400

    UI Updated

--- HandleChecker.py
+++ HandleChecker.py
@@ -29,23 +29,26 @@ layout = [
         ], vertical_alignment='center', justification='center'),
     ],
     [sg.Text("Enter handle:", size=(20, 1), justification='center', pad=((0, 0), (10, 0)), font=("Monospace", 14))],
-    [sg.Input(size=(25, 2), key='textbox', justification='center', font=("Monospace", 16))],
-    [sg.Button("Search", button_color='white', pad=((0, 0), (15,10)), font=("Monospace", 20))],
+    [sg.Input(size=(20, 4), key='textbox', justification='center', font=("Monospace", 16))],
+    [sg.Button("Search", button_color='white', pad=((0, 0), (10,15)), font=("Monospace", 14))],
 
     [
         sg.Column([
-            [sg.Text('FaceBook:', font=("Monospace", 20)), sg.Text('', key='result_FaceBook', font=("Monospace", 20))],
-            [sg.Text('Instagram:', font=("Monospace", 20)), sg.Text('', key='result_Instagram', font=("Monospace", 20))],
-            [sg.Text('TikTok:', font=("Monospace", 20)), sg.Text('', key='result_TikTok', font=("Monospace", 20))],
-            [sg.Text('Pinterest:', font=("Monospace", 20)), sg.Text('', key='result_Pinterest', font=("Monospace", 20))],
-            [sg.Text('YouTube:', font=("Monospace", 20)), sg.Text('', key='result_YouTube', font=("Monospace", 20))],
-            [sg.Text('Twitch:', font=("Monospace", 20)), sg.Text('', key='result_Twitch', font=("Monospace", 20))],
-        ], vertical_alignment='center', justification='left'),
+            [sg.Text('FaceBook:', font=("Monospace", 14)), sg.Text('', key='result_FaceBook', text_color='', font=("Monospace", 20))],
+            [sg.Text('Instagram:', font=("Monospace", 14)), sg.Text('', key='result_Instagram', font=("Monospace", 20))],
+            [sg.Text('TikTok:', font=("Monospace", 14)), sg.Text('', key='result_TikTok', font=("Monospace", 20))],
+        ], vertical_alignment='center', justification='center'),
+            
+        sg.Column([
+            [sg.Text('Pinterest:', font=("Monospace", 14)), sg.Text('', key='result_Pinterest', font=("Monospace", 20))],
+            [sg.Text('YouTube:', font=("Monospace", 14)), sg.Text('', key='result_YouTube', font=("Monospace", 20))],
+            [sg.Text('Twitch:', font=("Monospace", 14)), sg.Text('', key='result_Twitch', font=("Monospace", 20))],
+        ], vertical_alignment='center', justification='center'),
     ],
-    
+   [sg.Button(button_text='', pad=((220, 0), (0,15)), image_filename='info_icon.png', key= 'info_button', button_color='#323232', mouseover_colors='#323232')] 
 ]
 
-window = sg.Window("HandleChecker", layout, size=(300,465), element_justification='center')
+window = sg.Window("HandleChecker", layout, size=(300,380), element_justification='center')
 
 
 
@@ -85,9 +88,9 @@ def print_selected(key):
     result_key = f'result_{key}'
     if key != 'textbox':
         if checkhandle(handle, key):
-            window[result_key].update('✓')
+            window[result_key].update('✓', text_color = 'green')
         else:
-            window[result_key].update('✗')
+            window[result_key].update('✗', text_color = 'red')
 
             
 
@@ -96,6 +99,9 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
+
+    if event == 'info_button':
+         sg.popup('Some social medias may not accept special characters and may require a minimum amount of characters.\n\nRun the search a few times to get the most accurate results.', title='Disclaimer', font=("Monospace", 12), background_color='grey')
 
     for key, selected in values.items():
         if selected:
